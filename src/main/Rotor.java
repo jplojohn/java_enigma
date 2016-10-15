984766b6c48f87f2e31e43efff3f8098ba312e64
John Oliver <jpo14@imperial.ac.uk>
2016-10-15 16:19:31 +0100

    Rotors and RotorTests now working as expected! Created empty file for testing plugboard configs.

--- src/main/Rotor.java
+++ src/main/Rotor.java
@@ -7,39 +7,37 @@ public class Rotor {
 	private int position;
 	private int rotorNumber;
 	private int incrementCount;
-	
+
 	public Rotor (int[] map, int startPosition, int rotorNumber) {
 		this.map = map;
 		this.position = startPosition;
 		this.rotorNumber = rotorNumber;
 		this.incrementCount = 0;
 	}
-	
+
 	private void incrementPosition() {
-		incrementCount++;
-		
-		if (incrementCount % (26^rotorNumber) != 0) {
+        incrementCount++;
+
+		if ((incrementCount % (Math.pow(26,rotorNumber))) == 0) {
 			if (position >= 25) {
 				position = 0;
+				incrementCount = 0;
 			} else {
 				position++;
 			}
-		}
+
+        }
 	}
-	
+
 	public char translateForwards(char c) {
 		int charPos = alphabet.charToPosition(c);
-		int translatedCharPos = (map[charPos] + position) % 26;
+		int translatedCharPos = map[(charPos + position) % 26];
 		return alphabet.positionToChar(translatedCharPos);
 	}
-	
+
 	public char translateBackwards(char c) {
 		int charPos = alphabet.charToPosition(c);
-		int translatedCharPos = (array.search(map, charPos) + position) % 26;
-/*		System.out.println("charPos is: " + charPos + "\nPosition of charPos " +
-				"in reverse is " + array.search(map, charPos) + "\n" +
-				"Position of charPos in reverse + rotor position is: " +
-				array.search(map, charPos - position)); */
+		int translatedCharPos = ((array.search(map, charPos) - position + 26) % 26);
 		incrementPosition();
 		return alphabet.positionToChar(translatedCharPos);
 	}
