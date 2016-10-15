cca07e596cac5101b548dd81d29a3746f6b128ec
John Oliver <jpo14@imperial.ac.uk>
2016-10-15 16:30:43 +0100

    Plugboards now act as an identity mapping.

---
+++ src/main/Plugboard.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Plugboard {
+    private int[] map;
+
+    public Plugboard(int[] map) {
+
+    }
+
+    public char translate(char c) {
+        return c;
+    }
+}
--- src/test/PlugboardTest.java
+++ src/test/PlugboardTest.java
@@ -1,4 +1,25 @@
 package test;
 
+import main.Plugboard;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class PlugboardTest {
+
+    // Taken from the respective .pb files in ../../plugboards/
+    private int[] pb0 = {};
+    private int[] pb1 = {25,8};
+    private int[] pb2 = {25,10,22,9,21,4};
+
+    @Test
+    public void testEmptyPlugboard() {
+        Plugboard mypb0 = new Plugboard(pb0);
+        for (char c = 'A'; c <= 'Z'; c++) {
+            assertThat(mypb0.translate(c), is(c));
+
+        }
+    }
+
 }
